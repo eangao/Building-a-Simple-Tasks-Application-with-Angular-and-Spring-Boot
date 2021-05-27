8618aaebfa7aca20b27adfff3d688ed0f4010d4a
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-27 18:35:47 +0800

    Angular Components - close # 5

--- tasks/src/main/frontend/src/app/app.module.ts
+++ tasks/src/main/frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {TasksComponent} from './tasks/tasks.component';
+import {TasksListComponent} from './tasks/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
