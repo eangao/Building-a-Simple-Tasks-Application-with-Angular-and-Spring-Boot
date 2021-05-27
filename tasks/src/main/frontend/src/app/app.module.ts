ea38b7197f5671e6fe022bc890c94686d5a26835
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-27 19:10:11 +0800

    Bootstrap installed - close # 6.3

--- tasks/src/main/frontend/src/app/app.module.ts
+++ tasks/src/main/frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TasksComponent} from './tasks/tasks.component';
 import {TasksListComponent} from './tasks/tasks-list/tasks-list.component';
+import {TasksAddComponent} from './tasks/tasks-add/tasks-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
-    TasksListComponent
+    TasksListComponent,
+    TasksAddComponent
   ],
   imports: [
     BrowserModule,
